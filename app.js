31d77d56fd4793553d6fcdb84c24aa69057aa64c
maurizio <mauriziodm91@gmail.com>
2021-07-28 00:27:54 -0300

    changed some cdn and added some features to the app

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log('connected');
+//DEFINIENDO VARIABLES
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+//EVENT LISTENERS
+loadEventListeners();
+
+function loadEventListeners() {
+    form.addEventListener('submit', addTask);
+}
+
+//ADDTASK FUNCTION
+
+function addTask(e){
+    if(taskInput.value === ''){
+        alert('Agregue una tarea');
+    }
+
+    //CREANDO ELEMENTOS LI
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    //CREANDO NODO DE TEXTO Y ENLACE A LI
+    li.appendChild(document.createTextNode(taskInput.value));
+    //CREAR NUEVO ELEMENTO LINK
+    const link = document.createElement('a');
+    //AGREGAR SUS RESPECTIVAS CLASES A LINK
+    link.className = 'delete-item secondary-content';
+    //AGREGAR ICONO POR HTML
+    link.innerHTML = '<i class="fa fa-remove"></i>'
+    //AGREGAR LINK A ELEMENTO LI
+    li.appendChild(link);
+    //AGREGAR LI A UL
+    taskList.appendChild(li);
+    //BORRAR ENTRADA DE TAREAS
+    taskList.value = '';
+
+
+
+    e.preventDefault();
+}
